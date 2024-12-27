33648cc8eb965a39a8441b9920617e877a52eedf
vialfmj <vialfmj@gmail.com>
2024-12-26 22:48:59 -0300

    [fixn] - day7

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,12 +1,19 @@
 function fixPackages(packages: string): string {
   // Code here
-  let start: number;
-  let end: number;
-  end = packages.indexOf(")");
-  return "";
+
+  let result = packages;
+  while (result.includes("(")) {
+    let start = result.lastIndexOf("(");
+    let end = result.indexOf(")", start);
+    let s = result.slice(start + 1, end);
+    let s2 = s.split("").reverse().join("");
+
+    result = result.replace(result.substring(start, end + 1), s2);
+  }
+  return result;
 }
 
-const exampleInput7 = "a(cb)de";
+const exampleInput7 = "a(bc(def)g)h";
 
 console.log({ exampleInput: exampleInput7 });
 
