7cb6dca489bae9e215ba4f3ac3e7329beb1c7641
vialfmj <vialfmj@gmail.com>
2025-01-03 00:48:36 -0300

    [fix] - day8 errors

--- src/day8/index.ts
+++ src/day8/index.ts
@@ -1,22 +1,34 @@
 function drawRace(indices: number[], length: number): string {
-    let result = ''
-    indices.forEach((element, index, array) => {
-        let newString = ' '.repeat(array.length - index)+('~'.repeat(length) + '/' + (index + 1)  + '\n')        
-        if(element > 0)
-        newString = newString.substring(array.length - index, element) + 'r' + newString.substring(element, newString.length)
-       
-        result += newString
-    });
-    let s = '-------'.at(-1)
-    console.log(s)
-    return result
-  }
+  let result = "";
+  indices.forEach((element, index, array) => {
+    let newString = "";
+    if (element === 0) newString = "~".repeat(length);
+    else newString = "~".repeat(length - 1);
+    newString += " ";
+    if (element > 0)
+      newString =
+        newString.substring(0, element) +
+        "r" +
+        newString.substring(element, newString.length);
 
+    if (element < 0)
+      newString =
+        newString.substring(0, length + element) +
+        "r" +
+        newString.substring(length + element, newString.length);
 
-  
-  const exampleInput8 = {
-    indices: [0, 5, -3],
-    length: 10
-  }
+    newString = " ".repeat(array.length - index - 1) + newString;
 
-  console.log(drawRace(exampleInput8.indices, exampleInput8.length))
+    newString += "/" + (index + 1);
+    newString += index === array.length - 1 ? "" : "\n";
+    result += newString;
+  });
+  return result;
+}
+
+const exampleInput8 = {
+  indices: [0, 5, -3],
+  length: 10,
+};
+
+console.log(drawRace(exampleInput8.indices, exampleInput8.length));
